cc08b2e1afb16fe8688e6a31c455933ae5160d13
ALEN TL <67356843+ALENTL@users.noreply.github.com>
2021-01-07 16:07:24 +0530

    First

---
+++ translation.py
@@ -0,0 +1,29 @@
+class Translation(object):
+    START_TEXT = """ Simple Telegram Rename BOT Powered By GPLV3
+
+<b>Please send me any Telegram file and reply /rename New Name.extn</b>
+Searching For Help? 
+NO,Mate 
+No Help IS Provided🤷‍♂️🤷‍♂️🤣"""
+
+    RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
+    ABS_TEXT = "What Are You Trying To Do,Mate?"
+    UPGRADE_TEXT = "CONTACT @AID_3N"
+    DOWNLOAD_START = "Trying to Download......"
+    UPLOAD_START = "Trying to Upload......."
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me😒😒**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
+    NOT_AUTH_USER_TEXT = "CONTACT @AID_3N"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @AID_3N"
+    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail saved ✅ This Will Be Used For One File Only"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully😎"
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found,SED🤒"
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    HELP_USER = """Hai {}
+    
+1. Send me any Telegram File.
+2. Reply to that message to /rename NEW NAME.EXTENSION"""
